240857616819b18ca419d10d0640296de283b3cb
Gilbert Cuerbo <gilbert.cuerbo@gmail.com>
2021-02-04 04:18:45 +0800

    add jwt test

--- services/token.js
+++ services/token.js
@@ -1,14 +1,22 @@
 const jwt = require('jsonwebtoken');
 
-const EXPIRE = 24
+const EXPIRE = 24 // hours
 const TEST_SECRET = 'secret-key-101'
 
 module.exports ={
     generate(data) {
-
+        return jwt.sign(data, TEST_SECRET,{expiresIn: EXPIRE * 3600})
     },
 
-    verify(token) {
-
+    async verify(token) {
+        return new Promise((resolve, reject) => {
+            jwt.verify(token, TEST_SECRET, function(err, decoded){
+                if(!err){
+                    resolve(decoded)
+                } else {
+                    reject(err)
+                }
+            })
+        })
     }
 }
